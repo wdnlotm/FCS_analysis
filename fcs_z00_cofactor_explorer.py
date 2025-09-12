48ac719295b65386384725b2b69aba563d4a03ed
Myles Kim <90907360+wdnlotm@users.noreply.github.com>
2025-09-11 21:26:52 -0400

    Update fcs_z00_cofactor_explorer.py

--- fcs_z00_cofactor_explorer.py
+++ fcs_z00_cofactor_explorer.py
@@ -2,8 +2,6 @@ import os
 import time
 import argparse
 import subprocess
-# import bokeh
-# from bokeh.plotting import show
 
 import sys
 import yaml
@@ -36,20 +34,9 @@ color_list = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8',\
         '#800000', '#808000', '#2f4f4f', '#a9a9a9',\
         '#ffd8b1', '#000000', '#fffac8', '#aaffc3']
 
+from fcssupport import init_pool, unpack_and_run_nogroup, plot_density_nogroup
+
 ## Functions
-
-def init_pool(the_results):
-  global results
-  results = the_results
-
-
-def unpack_and_run(args):
-    """Helper function to unpack arguments and call the main function."""
-    return plot_density_nogroup_noglob(*args)
-
-def unpack_and_run_group(args):
-    """Helper function to unpack arguments and call the main function."""
-    return plot_density_grouped(*args)
 
 #add 'sample id; coloumns
 def augmented_df(df, sid):
@@ -57,88 +44,6 @@ def augmented_df(df, sid):
     return df
 
 
-## density plot grouped by sample id
-def plot_density_grouped(ii, group_by, fcs_df_trimmed, cofactors, figure_dir, dataset_name):
-    """ Function that plots marker expressiong density 
-    plot grouped by sample_id for each marker with various cofactors"""
-    # dataset_name='spect'
-    bw_val=0.5
-    frac_value=0.15
-    alpha_val=0.5
-    linewidth_val=1
-    plt.rcParams['font.size'] = 8 # Set default font size
-    num_col=5 #int(np.ceil(np.sqrt(fcs_df_trimmed.shape[1]-1)))
-    num_row=int(np.ceil(len(cofactors)/num_col))
-    fig, axes = plt.subplots(num_row, num_col, 
-                         figsize=(2.0*num_col, 1.2*num_row) ) # 1 row, 2 columns
-    marker = fcs_df_trimmed.columns[ii]
-
-    for cf in cofactors:
-        jj=cofactors.index(cf)
-        row_index= jj//num_col
-        col_index= jj%num_col
-        
-        fcs_df_trimmed_onemarker = fcs_df_trimmed[[marker, group_by]].copy()
-        fcs_df_trimmed_onemarker = fcs_df_trimmed_onemarker.sample(frac=frac_value)
-        fcs_df_trimmed_onemarker[marker] = np.arcsinh(fcs_df_trimmed_onemarker[marker]/cf)
-        sns.kdeplot(data=fcs_df_trimmed_onemarker, x=marker, bw_adjust=bw_val, hue=group_by, 
-            legend=False, ax=axes[row_index,col_index], alpha=alpha_val, linewidth=linewidth_val)
-        axes[row_index,col_index].set_title(f"cofactor {cf}", fontsize=10)
-
-    plt.tight_layout()
-    filename=f'{dataset_name}_marker_{marker}_bw_{bw_val}_group_by_subjects.png'
-    plt.savefig(f'{figure_dir}/{filename}')    
-    plt.close('all')
-    
-    return filename
-
-
-## density plot no groupping
-def plot_density_nogroup_noglob(ii, fcs_df_trimmed, cofactors, figure_dir, dataset_name):
-    """ Function that plots marker expressiong density 
-    plot for each marker with various cofactors"""
-    # dataset_name='spect'
-    bw_val=0.5
-    frac_value=0.15
-    alpha_val=0.5
-    linewidth_val=1
-    plt.rcParams['font.size'] = 8 # Set default font size
-    
-    num_col=5 #int(np.ceil(np.sqrt(fcs_df_trimmed.shape[1]-1)))
-    num_row=int(np.ceil(len(cofactors)/num_col))
-    fig, axes = plt.subplots(num_row, num_col, 
-                         figsize=(2.0*num_col, 1.2*num_row) ) # 1 row, 2 columns
-    
-    marker = fcs_df_trimmed.columns[ii]
-    # print(marker)
-
-    for cf in cofactors:
-        jj=cofactors.index(cf)
-        row_index= jj//num_col
-        col_index= jj%num_col
-        
-        # print(fcs_df_trimmed.head(2))
-        
-        fcs_df_trimmed_onemarker = fcs_df_trimmed[[marker, 'sample_id']].copy()
-        fcs_df_trimmed_onemarker = fcs_df_trimmed_onemarker.sample(frac=frac_value)
-        fcs_df_trimmed_onemarker[marker] = np.arcsinh(fcs_df_trimmed_onemarker[marker]/cf)
-        sns.kdeplot(data=fcs_df_trimmed_onemarker, x=marker, bw_adjust=bw_val, #hue='sample_id', 
-            legend=False, ax=axes[row_index,col_index], alpha=alpha_val, linewidth=linewidth_val)
-        axes[row_index,col_index].set_title(f"cofactor {cf}", fontsize=10)
-
-    # print('I am here')
-    plt.tight_layout()
-    filename=f'{dataset_name}_marker_{marker}_bw_{bw_val}_no_group.png'
-    # print(filename)
-    
-    # print(f'{figure_dir}/{filename}')
-    plt.savefig(f'{figure_dir}/{filename}')    
-    plt.close('all')
-    
-    return filename
-
-
-## Functions - DONE
 
 ## Main - take inputs
 ##        1. figure_dir_name
@@ -154,44 +59,40 @@ if __name__ == "__main__":
     manager = Manager()
     results = manager.list()
 
-    parser = argparse.ArgumentParser(description='high dimensional cytomery data z00 cofactor exploration')
-    parser.add_argument('-uyml','--useyaml', type=bool, default=True, help='decide to use yaml or argparse')
-    parser.add_argument('-conf','--config', type=str, default="config.yaml", help='configuration yaml file')
-    parser.add_argument('-dl','--dataloc', type=str, default="../_Allcell/_subsample_100k_bcell/", help='data location')
-    parser.add_argument('-fsd','--figuresavedir', type=str, default="figures_for_spectral_cofactor", help='figure save dir')
-    parser.add_argument('-cfs','--cofactorlist', type=str, default="[1000.0, 2000.0, 3000.0]", help='cofactor list')
-    parser.add_argument('-ds','--dataset', type=str, default="spectral", help='dataset name')
-    parser.add_argument('-pbs','--plotbysubj', type=bool, default=False, help='True: will make density plot by subject')
-    parser.add_argument('-np','--numpool', type=int, default=10, help='Number of cpu in nogroup plots')
-    args, unknown = parser.parse_known_args()
+    # parser = argparse.ArgumentParser(description='high dimensional cytomery data z00 cofactor exploration')
+    # parser.add_argument('-uyml','--useyaml', type=bool, default=True, help='decide to use yaml or argparse')
+    # parser.add_argument('-conf','--config', type=str, default="config.yaml", help='configuration yaml file')
+    # parser.add_argument('-dl','--dataloc', type=str, default="../_Allcell/_subsample_100k_bcell/", help='data location')
+    # parser.add_argument('-fsd','--figuresavedir', type=str, default="figures_for_spectral_cofactor", help='figure save dir')
+    # parser.add_argument('-cfs','--cofactorlist', type=str, default="[1000.0, 2000.0, 3000.0]", help='cofactor list')
+    # parser.add_argument('-ds','--dataset', type=str, default="spectral", help='dataset name')
+    # parser.add_argument('-pbs','--plotbysubj', type=bool, default=False, help='True: will make density plot by subject')
+    # parser.add_argument('-np','--numpool', type=int, default=10, help='Number of cpu in nogroup plots')
+    # args, unknown = parser.parse_known_args()
     
-    if not args.useyaml:
-        figure_dir = args.figuresavedir #'figures_for_spectral_cofactor'
-        dataloc = args.dataloc
-        cofactors = eval(args.cofactorlist)  #[x*1000.0 for x in range(1,16)]+[20000.0, 30000.0]
-        dataset_name = args.dataset
-        plotbysubj = args.plotbysubj
-        numpool = args.numpool
+    # if not args.useyaml:
+    #     figure_dir = args.figuresavedir #'figures_for_spectral_cofactor'
+    #     dataloc = args.dataloc
+    #     cofactors = eval(args.cofactorlist)  #[x*1000.0 for x in range(1,16)]+[20000.0, 30000.0]
+    #     dataset_name = args.dataset
+    #     plotbysubj = args.plotbysubj
+    #     numpool = args.numpool
 
     
-    if args.useyaml:
-        with open('config.yaml', 'r') as f:
-            config = yaml.safe_load(f)
-            figure_dir = config['parameters']['z00']['figure_dir'] # figure_dir: where to save figures
-            dataloc = config['parameters']['z00']['dataloc']       # data location
-            cofactors = eval(config['parameters']['z00']['cofactorlist'])
-            dataset_name = config['parameters']['z00']['dataset']
-            plotbysubj = config['parameters']['z00']['plotbysubj']
-            numpool =  config['parameters']['z00']['numpool']
-            savefile_fcs_trimmed = config['parameters']['z00']['savefile_fcs_trimmed']
+    # if args.useyaml:
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+        figure_dir = config['z00']['figure_dir'] # figure_dir: where to save figures
+        dataloc = config['z00']['dataloc']       # data location
+        cofactors = eval(config['z00']['cofactorlist'])
+        dataset_name = config['z00']['dataset']
+        plotbysubj = config['z00']['plotbysubj']
+        numpool =  config['z00']['numpool']
+        savefile_fcs_trimmed = config['z00']['savefile_fcs_trimmed']
             
-    print(config['parameters']['z00'])
-    
-  
-    
-    if not os.path.isdir(figure_dir):
-        os.mkdir(figure_dir)
-    
+    print(config['z00'])
+
+    os.makedirs(f'{figure_dir}', exist_ok=True)
 
     samples_all = fk.load_samples(dataloc)
     
@@ -248,20 +149,20 @@ if __name__ == "__main__":
     fcs_df_trimmed = fcs_df.iloc[:,marker_col]
     print(fcs_df_trimmed.head())
     
+    print(f'Saving trimmed fcs data: {savefile_fcs_trimmed}')
     fcs_df_trimmed.to_csv(savefile_fcs_trimmed, index=False, compression='gzip')
 
-    # global num_markers
-    num_markers=fcs_df_trimmed.shape[1]-1
+    print(f'Saving trimmed fcs data done!!!')
     
-
-    inputs = [(ii, fcs_df_trimmed, cofactors, figure_dir, dataset_name) for ii in range(num_markers)]
+    global num_markers
+    num_markers=fcs_df_trimmed.shape[1]-1
+    marker_list = fcs_df_trimmed.columns[0:num_markers]
+    print(marker_list)
+    print(len(marker_list))
+    
+    print(f'Making plots')
+    inputs = [(m, fcs_df_trimmed, cofactors, figure_dir, dataset_name) for m in marker_list]
     
     with multiprocessing.get_context('spawn').Pool(numpool, initializer=init_pool, initargs=(results,)) as pool:
-        list(tqdm(pool.imap_unordered(unpack_and_run, inputs), total=len(inputs)))
+        list(tqdm(pool.imap_unordered(unpack_and_run_nogroup, inputs), total=len(inputs)))
 
-
-    if plotbysubj:
-        inputs = [(ii, 'sample_id', fcs_df_trimmed, cofactors, figure_dir, dataset_name) for ii in range(num_markers)]
-        # Somehow multiprocessing is slower than single processing. Keep 1 for the process count
-        with multiprocessing.get_context('spawn').Pool(1, initializer=init_pool, initargs=(results,)) as pool:
-            list(tqdm(pool.imap_unordered(unpack_and_run_group, inputs), total=len(inputs)))
